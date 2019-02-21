90712d415d1a5251c8e60d62c10e98ca6e7acc53
unknown <russel.tsang@gmail.com>
2019-02-20 22:01:53 -0500

    raised counters state from counters.jsx -> app.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,59 @@ import "./App.css";
 import Counters from "./components/ReactCourseSection2/counters";
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 4 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ]
+  };
+
+  //set all "values" of "counters" object to zero
+  handleReset = () => {
+    const counters = this.state.counters.map(counter => {
+      counter.value = 0;
+      return counter;
+    })
+    this.setState({ counters });
+  }
+
+  //increment value of selected counter by 1
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  //decrement value of selected counter by 1
+  handleDecrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
+    this.setState({ counters });
+  };
+
+  //deletes selected counter
+  handleDelete = (productID) => {
+    const counters = this.state.counters.filter(nthCounter => nthCounter.id !== productID);
+    this.setState({ counters: counters });  //also can just use one "counter", since key + value are the same
+  };
+
   render() {
     return (
       <React.Fragment>
-        <NavBar />
+        <NavBar totalCounters={this.state.counters.filter(nthCounter => nthCounter.value > 0).length} />
         <main className="container">
-          <Counters />
+          <Counters
+            handleReset={this.handleReset}
+            handleIncrement={this.handleIncrement}
+            handleDecrement={this.handleDecrement}
+            handleDelete={this.handleDelete}
+            counters={this.state.counters} />
         </main>
       </React.Fragment>
     );
--- src/components/ReactCourseSection2/counters.jsx
+++ src/components/ReactCourseSection2/counters.jsx
@@ -1,60 +1,22 @@
 import React, { Component } from 'react';
-import Counter2 from "../ReactCourseSection1/counter1";
+import Counter1 from "../ReactCourseSection1/counter1";
 
 class Counters extends Component {
-    state = {
-        counters: [
-            { id: 1, value: 0 },
-            { id: 2, value: 4 },
-            { id: 3, value: 0 },
-            { id: 4, value: 0 },
-        ]
-    };
-
-    handleReset = () => {
-        const counters = this.state.counters.map(counter => {
-            counter.value = 0;
-            return counter;
-        })
-        this.setState({ counters });
-    }
-
-    handleIncrement = counter => {
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index] = { ...counter };
-        counters[index].value++;
-        this.setState({ counters });
-    };
-
-    handleDecrement = counter => {
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index] = { ...counter };
-        counters[index].value--;
-        this.setState({ counters });
-    };
-
-    handleDelete = (productID) => {
-        const counters = this.state.counters.filter(nthCounter => nthCounter.id !== productID);
-        this.setState({ counters: counters });  //we can also just use one "counter", since key + value are the same
-    };
-
     render() {
         return (
             <div>
                 <button
-                    onClick={this.handleReset}
+                    onClick={this.props.handleReset}
                     className="btn btn-primary btn-sm m-2">Reset</button>
-                {this.state.counters.map(counter => (
-                    <Counter2
-                        handleIncrement={this.handleIncrement}
-                        handleDecrement={this.handleDecrement}
-                        onDelete={this.handleDelete}
+                {this.props.counters.map(counter => (
+                    <Counter1
+                        handleIncrement={this.props.handleIncrement}
+                        handleDecrement={this.props.handleDecrement}
+                        onDelete={this.props.handleDelete}
                         key={counter.id}
                         counter={counter} >
                         <h4>Counter Id, {counter.id}</h4>
-                    </Counter2>
+                    </Counter1>
                 )
                 )}
             </div >
