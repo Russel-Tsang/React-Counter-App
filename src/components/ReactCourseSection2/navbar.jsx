e2387aed519d27d2b027ba3d28b446bcc97f041b
unknown <russel.tsang@gmail.com>
2019-02-22 23:56:16 -0500

    changed navbar to stateless function component

--- src/components/ReactCourseSection2/navbar.jsx
+++ src/components/ReactCourseSection2/navbar.jsx
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 
-class navbar extends Component {
-    render() {
-        return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">
-                    Navbar <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
-                </a>
-            </nav>
-        );
-    }
+//navigation bar that displays the number of counters
+const navbar = (props) => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+                Navbar <span className="badge badge-pill badge-secondary">{props.totalCounters}</span>
+            </a>
+        </nav>
+    );
 }
 
 export default navbar;
