c58f27f4af14c5975d4c48652f3d804b13551e49
unknown <russel.tsang@gmail.com>
2019-02-22 23:59:20 -0500

    destructured component

--- src/components/ReactCourseSection2/counters.jsx
+++ src/components/ReactCourseSection2/counters.jsx
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import Counter1 from "../ReactCourseSection1/counter1";
 
+//reset button + a number of counters (based on the how many counters are in the counters state)
 class Counters extends Component {
     render() {
+        const { handleReset, handleIncrement, handleDecrement, handleDelete } = this.props;
         return (
             <div>
                 <button
-                    onClick={this.props.handleReset}
+                    onClick={handleReset}
                     className="btn btn-primary btn-sm m-2">Reset</button>
                 {this.props.counters.map(counter => (
                     <Counter1
-                        handleIncrement={this.props.handleIncrement}
-                        handleDecrement={this.props.handleDecrement}
-                        onDelete={this.props.handleDelete}
+                        handleIncrement={handleIncrement}
+                        handleDecrement={handleDecrement}
+                        onDelete={handleDelete}
                         key={counter.id}
                         counter={counter} >
                         <h4>Counter Id, {counter.id}</h4>
